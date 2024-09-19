fc618ad99cd8d98e1b649e040325bc022f514a8c
Pathompong <Pathompong.forwork@gmail.com>
2024-09-19 13:17:48 +0700

    [#19-09-2024] Add handlesignup

--- frontend/src/pages/auth/index.jsx
+++ frontend/src/pages/auth/index.jsx
@@ -1,7 +1,56 @@
 import Background from "@/assets/login2.png";
 import Victory from "../../assets/victory.svg";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { useState } from "react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
+import { apiClient } from "@/lib/apiClient";
+import { SIGNUP_ROUTE } from "@/utils/constants";
 const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const validateSignup = () => {
+    if (!email.length) {
+      toast.error("Email is required.");
+      return false;
+    }
+    if (!password.length) {
+      toast.error("Password is required.");
+      return false;
+    }
+    if (password !== confirmPassword) {
+      toast.error("Passwords do not match.");
+      return false;
+    }
+    return true;
+  };
+  const handleLogin = () => {
+    console.log(email, password);
+  };
+  const handleSignup = async () => {
+    if (validateSignup()) {
+      console.log("validate pass");
+      try {
+        await apiClient.post(
+          SIGNUP_ROUTE,
+          { email, password }
+          // { withCredentials: true }
+        );
+      } catch (error) {
+        console.log("Error", error.response);
+        toast.error(error.response.data);
+      }
+
+      // if (response.status === 201) {
+      //   toast.success("Signup successful");
+      // } else {
+      //   toast.error("Signup failed");
+      // }
+    }
+  };
   return (
     <div className="h-[100vh] w-[100vw] flex items-center justify-center">
       <div className="h-[80vh] bg-white border-2 border-white text-opacity-90 shadow-2xl w-[80vw] md:w-[90vw] lg:w-[70vw] xl:w-[60vw] rounded-3xl grid xl:grid-cols-2">
@@ -16,8 +65,8 @@ const Auth = () => {
             </p>
           </div>
           <div className="flex items-center justify-center w-full">
-            <Tabs>
-              <TabsList className="bg-transparent r">
+            <Tabs className="w-3/4">
+              <TabsList className="bg-transparent rounded-none w-full">
                 <TabsTrigger
                   value="login"
                   className="data-[state=active]:bg-transparent text-black text-opacity-90 border-b-2 rounded-none w-full data-[state=active]:text-black data-[state=active]:font-semibold data-[state=active]:border-b-purple-500 p-3 transition-all duration-300"
@@ -31,10 +80,54 @@ const Auth = () => {
                   Signup
                 </TabsTrigger>
               </TabsList>
-              <TabsContent value="login" className=""></TabsContent>
-              <TabsContent value="signup" className=""></TabsContent>
+              <TabsContent value="login" className="flex flex-col gap-5 mt-10">
+                <Input
+                  placeholder="Email"
+                  type="email"
+                  className="rounded-full p-6"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <Input
+                  placeholder="Password"
+                  type="password"
+                  className="rounded-full p-6"
+                  value={email}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <Button className="rounded-full p-6">Login</Button>
+              </TabsContent>
+              <TabsContent value="signup" className="flex flex-col gap-5">
+                <Input
+                  placeholder="Email"
+                  type="email"
+                  className="rounded-full p-6"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <Input
+                  placeholder="Password"
+                  type="password"
+                  className="rounded-full p-6"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <Input
+                  placeholder="Confirm Password"
+                  type="password"
+                  className="rounded-full p-6"
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                />
+                <Button className="rounded-full p-6" onClick={handleSignup}>
+                  Signup
+                </Button>
+              </TabsContent>
             </Tabs>
           </div>
+        </div>
+        <div className="hidden xl:flex justify-center items-center">
+          <img src={Background} className="h-[700px]" alt="background login" />
         </div>
       </div>
     </div>
